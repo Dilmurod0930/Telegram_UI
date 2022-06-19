3ea5be8a658187961f1bde7b0e295d37711d714a
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2022-06-19 21:12:09 +0500

    Contacts add

--- lib/screens/auth/view/auth_view.dart
+++ lib/screens/auth/view/auth_view.dart
@@ -17,6 +17,16 @@ class AuthView extends StatelessWidget {
       create: (context) => AuthCubit(),
       child: Scaffold(
         appBar: AppBar(
+          leading:  TextButton(
+              child: Text('Cancel'.tr()),
+              onPressed: () {
+                Navigator.pushNamedAndRemoveUntil(
+                  context,
+                  '/cart',
+                  (route) => false,
+                );
+              },
+            ),
           backgroundColor: Colors.transparent,
           elevation: 0,
           actions: [
@@ -38,7 +48,7 @@ class AuthView extends StatelessWidget {
             controller.text = context.watch<AuthCubit>().phoneCityCode;
             return Column(
               children: [
-                Text('Your_P'.tr(), style: const TextStyle(fontSize: 30)),
+                Text('Your_Phone'.tr(), style: const TextStyle(fontSize: 30)),
                 const SizedBox(height: 17),
                 SizedBox(
                   height: 38,
@@ -52,7 +62,7 @@ class AuthView extends StatelessWidget {
                     value: context.watch<AuthCubit>().selectedLocation,
                     hint: Text(context.watch<AuthCubit>().selectedLocation),
                     onChanged: (v) => context.read<AuthCubit>().valueDrop(v!),
-                    items: <String>['USA', 'RUS', 'UZB'].map((String value) {
+                    items: <String>['USA', 'UZB','RUS', ].map((String value) {
                       return DropdownMenuItem<String>(
                         value: value,
                         child: Text(value),
@@ -60,23 +70,26 @@ class AuthView extends StatelessWidget {
                     }).toList(),
                   ),
                 ),
-                Row(
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.25,
-                      child: TextField(
-                        controller: controller,
-                        onChanged: (v) {
-                          context.read<AuthCubit>().phoneCityCode = v;
-                          context.read<AuthCubit>().codeCity(v);
-                        },
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.20,
+                        child: TextField(
+                          controller: controller,
+                          onChanged: (v) {
+                            context.read<AuthCubit>().phoneCityCode = v;
+                            context.read<AuthCubit>().codeCity(v);
+                          },
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.75,
-                      child: TextFormField(),
-                    ),
-                  ],
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.75,
+                        child: TextFormField(),
+                      ),
+                    ],
+                  ),
                 ),
                 const SizedBox(height: 24.5),
                 Padding(
--- lib/screens/cart/pages/contacts/contacts_view.dart
+++ lib/screens/cart/pages/contacts/contacts_view.dart
@@ -1,10 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:telegram_ui/core/constants/color_const.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:telegram_ui/mock/chats_data.dart';
+import 'package:telegram_ui/screens/auth/cubit/auth_cubit.dart';
 
 class ContactsView extends StatelessWidget {
   const ContactsView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return BlocProvider(
+      create: (context) => AuthCubit(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: ColorsConst.colorWhite,
+          centerTitle: true,
+          elevation: 0,
+          title:
+              Text("Contacts", style: TextStyle(color: ColorsConst.colorBlack)),
+          actions: [
+            TextButton(
+              child: Text('+'.tr(), style: const TextStyle(fontSize: 32)),
+              onPressed: () {
+                Navigator.pushNamedAndRemoveUntil(
+                    context, '/cart', (route) => false);
+              },
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: ListView.builder(
+                  itemBuilder: (_, __) {
+                    return Column(
+                      children: [
+                        InkWell(
+                          child: ListTile(
+                            leading: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: AssetImage(
+                                  'assets/images/users/${ChatsData.chatsDate[__]['image']}.png'),
+                            ),
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(ChatsData.chatsDate[__]['name']),
+                               
+                              ],
+                            ),
+                            
+                          ),
+                          onLongPress: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) => alertDialog(context));
+                          },
+                        ),
+                        Divider()
+                      ],
+                    );
+                  },
+                  itemCount: ChatsData.chatsDate.length,
+                ),
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  AlertDialog alertDialog(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: ColorsConst.color858E96.withAlpha(240),
+      insetPadding: EdgeInsets.zero,
+      contentPadding: const EdgeInsets.all(8),
+      actionsAlignment: MainAxisAlignment.start,
+      content: Padding(
+        padding: const EdgeInsets.only(left: 11, right: 11, top: 76),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 1,
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.62,
+                width: MediaQuery.of(context).size.width * 1,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      actions: [
+        Container(
+          margin: const EdgeInsets.only(left: 10),
+          height: MediaQuery.of(context).size.height * 0.21,
+          width: MediaQuery.of(context).size.width * 0.66,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: ColorsConst.colorFEFEFE,
+          ),
+          child: Column(
+            children: [
+              a('Mark as Unread', ''),
+              const Divider(),
+              a('Mark as Unread', ''),
+              const Divider(),
+              a('Mark as Unread', ''),
+              const Divider(),
+              a('Mark as Unread', ''),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row a(text, nameSVG) {
+    return Row(
+      children: [
+        Text('$text'.tr()),
+        SvgPicture.asset('assets/icons/$nameSVG.svg')
+      ],
+    );
   }
 }
